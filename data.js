4ecc9149a5d8cb67012c6340a174ac4e64e85d11
anydeskforyou <anydeskforyou@gmail.com>
2026-09-06 13:28:07 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+const updates = {
+    jobs: [
+        {
+            title: "नई सरकारी नौकरी",
+            organization: "Government Department",
+            lastDate: "30 September 2026",
+            link: "#"
+        }
+    ],
+
+    results: [
+        {
+            title: "Latest Government Result",
+            organization: "Government Department",
+            date: "06 September 2026",
+            link: "#"
+        }
+    ],
+
+    admitCards: [
+        {
+            title: "Latest Admit Card",
+            organization: "Government Department",
+            examDate: "September 2026",
+            link: "#"
+        }
+    ],
+
+    answerKeys: [
+        {
+            title: "Latest Answer Key",
+            organization: "Government Department",
+            date: "06 September 2026",
+            link: "#"
+        }
+    ]
+};
